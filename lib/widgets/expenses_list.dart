a5755bd68578ad3a030910f129b58e6410f69b1d
ahmedmohsen302 <ahmedthemohsen@gmail.com>
2025-04-23 19:09:52 +0200

    created expenses list

---
+++ lib/widgets/expenses_list.dart
@@ -0,0 +1,15 @@
+import 'package:expenses_tracker_app/models/expenses_model.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({super.key, required this.expenses});
+  final List<ExpensesModel> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (context, index) => Text(expenses[index].title),
+    );
+  }
+}
