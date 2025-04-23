de6fc727e0b11e5aff7267ef71336c7e1b07f548
ahmedmohsen302 <ahmedthemohsen@gmail.com>
2025-04-23 19:06:23 +0200

    created dummy data

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:expenses_tracker_app/models/expenses_model.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatefulWidget {
@@ -10,6 +11,26 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
+    List<ExpensesModel> expense = [
+      ExpensesModel(
+        title: 'pizza',
+        amount: 150,
+        date: DateTime.now(),
+        category: Category.food,
+      ),
+      ExpensesModel(
+        title: 'cinema',
+        amount: 200,
+        date: DateTime.now(),
+        category: Category.liesure,
+      ),
+      ExpensesModel(
+        title: 'tour',
+        amount: 1000,
+        date: DateTime.now(),
+        category: Category.travel,
+      ),
+    ];
     return Scaffold(body: Column(children: [
 
         ],
